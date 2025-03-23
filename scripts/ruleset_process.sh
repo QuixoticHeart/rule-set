cbbf852cf136b15888e77fd49bcdb8c9f518f8ca
QuixoticHeart <wh111t3zz@gmail.com>
2025-03-23 16:06:04 +0800

    Chore: optimize workflow

--- scripts/ruleset_process.sh
+++ scripts/ruleset_process.sh
@@ -42,3 +42,29 @@ domain_dedupe(){
         }
     }' $1
 }
+
+ruleset_sort(){
+    awk '
+        /^\s*[#;]/ { next }
+        /^$/ { next }
+        sub(" //.*", "");
+        /^DOMAIN,/          { print "0 " $0; next }
+        /^DOMAIN-SUFFIX,/   { print "1 " $0; next }
+        /^DOMAIN-KEYWORD,/  { print "2 " $0; next }
+        /^DOMAIN-WILDCARD,/ { print "3 " $0; next }
+        /^DOMAIN-REGEX,/    { print "4 " $0; next }
+        /^IP-CIDR,/         { print "5 " $0; next }
+        /^IP-CIDR6,/        { print "6 " $0; next }
+        /^IP-ASN,/          { print "7 " $0; next }
+        /^PROCESS-NAME,/    { print "8 " $0; next }
+        /^GEOSITE,/         { print "9 " $0; next }
+        /^GEOIP,/           { print "10 " $0; next }
+        /^AND,/             { print "11 " $0; next }
+        /^OR,/              { print "12 " $0; next }
+        /^NOT,/             { print "13 " $0; next }
+        /^SUB-RULE,/        { print "14 " $0; next }
+        /^DEST-PORT,/       { print "15 " $0; next }
+        /^DST-PORT,/        { print "16 " $0; next }
+                            { print "17 " $0 }
+    ' $1 | sort -k1,1n -k2,2 | cut -d' ' -f2-
+}
