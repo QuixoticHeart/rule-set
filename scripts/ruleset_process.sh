f0a7edf628d69f73f9d37d2b4c96b00068c6f575
QuixoticHeart <wh111t3zz@gmail.com>
2025-03-28 13:53:52 +0800

    Fix: add robustness judgment

--- scripts/ruleset_process.sh
+++ scripts/ruleset_process.sh
@@ -27,7 +27,7 @@ domain_dedupe(){
         for (suffix in suffixes) {
             matched = 0;
             for (i = 1; i <= length(suffix); i++) {
-                if (substr(suffix, i) in suffixes && substr(suffix, i) != suffix && substr(suffix,i - 1, 1) == ".") {
+                if (substr(suffix, i) in suffixes && substr(suffix, i) != suffix && substr(suffix, i - 1, 1) == ".") {
                     matched = 1;
                     break;
                 }
@@ -45,9 +45,6 @@ domain_dedupe(){
 
 ruleset_sort(){
     awk '
-        /^\s*[#;]/ { next }
-        /^$/ { next }
-        sub(" //.*", "");
         /^DOMAIN,/          { print "0 " $0; next }
         /^DOMAIN-SUFFIX,/   { print "1 " $0; next }
         /^DOMAIN-KEYWORD,/  { print "2 " $0; next }
@@ -62,9 +59,7 @@ ruleset_sort(){
         /^AND,/             { print "11 " $0; next }
         /^OR,/              { print "12 " $0; next }
         /^NOT,/             { print "13 " $0; next }
-        /^SUB-RULE,/        { print "14 " $0; next }
-        /^DEST-PORT,/       { print "15 " $0; next }
-        /^DST-PORT,/        { print "16 " $0; next }
-                            { print "17 " $0 }
+        /^DEST-PORT,/       { print "14 " $0; next }
+        /^DST-PORT,/        { print "15 " $0; next }
     ' $1 | sort -k1,1n -k2,2 | cut -d' ' -f2-
 }
